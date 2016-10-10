3d07ac177040573bf84da8198a13233eccaeebb2
takuo hada <t_hada@dachicraft.net>
2016-10-10 22:15:54 +0900

    min/maxの内部ロジック入れ替え

--- src/src/decimal_min.c
+++ src/src/decimal_min.c
@@ -9,11 +9,20 @@
 
 MG_DECIMAL_API mg_decimal_error mg_decimal_min(const mg_decimal *value1, const mg_decimal *value2, /*out*/mg_decimal *ret)
 {
-	if(mg_decimal_compare_1(value1, value2) < 0) {
-		*ret = *value1;
-	} else {
+	mg_decimal_error err;
+	int ret1;
+
+	err = mg_decimal_compare(value1, value2, /*out*/&ret1);
+	if(err != 0)
+		goto _ERROR;
+
+	if(ret1 > 0) {
 		*ret = *value2;
+	} else {
+		*ret = *value1;
 	}
 
 	return 0;
+_ERROR:
+	return err;
 }
